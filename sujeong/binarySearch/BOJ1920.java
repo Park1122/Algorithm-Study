d436a21253c7c8cf5b49e227974d54659c1b9d0c
portfolio_1st <aa991122aa@gmail.com>
2021-07-19 19:14:56 +0900

    Solve 1920-수정

--- sujeong/binarySearch/BOJ1920.java
+++ sujeong/binarySearch/BOJ1920.java
@@ -30,25 +30,30 @@ public class BOJ1920 {
     }
 
     private static void func(){
+        // binarySearch를 제대로 사용하지 못한 듯함....
+        // 이 방식은 완전 탐색에 더 가깝다.
+        // 풀고나서 다른사람들과 비교하니 보임.
+//        for(int i=0;i<m;i++){
+//            for(int j=0;j<n;j++){
+//                if(arrBList[i]==arrAList[j]){
+//                    answer[i]=1;
+//                    break;
+//                } else if (arrBList[i]<arrAList[j]){
+//                    answer[i]=0;
+//                    break;
+//                }
+//            }
+//        }
+
         for(int i=0;i<m;i++){
-            boolean b=false;
-            for(int j=0;j<n;j++){
-                if(arrBList[i]==arrAList[j]){
-                    b=true;
-                    break;
-                } else if (arrBList[i]<arrAList[j]) break;
-            }
-            if(b) answer[i]=1;
-            else answer[i]=0;
+            answer[i]=Arrays.binarySearch(arrAList,arrBList[i])<0?0:1;
         }
+
     }
 
     private static void output() {
-        for(int i : answer){
-            System.out.println(i);
-        }
+        for(int i : answer) System.out.println(i);
     }
-
 
     public static void main(String[] args) throws IOException {
         input();
