c2ebbe14fc759549f57823567b9edb297fa1a63d
윤예은 <60867063+yeeun-yun97@users.noreply.github.com>
2021-09-10 22:36:40 +0900

    Solve 2688-예은

--- yeeun/dp/BOJ2688.java
+++ yeeun/dp/BOJ2688.java
@@ -3,12 +3,14 @@ package codingstudy202107.w10;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /**
  * 회차 : 코딩 스터디 10주차
  * 문제 출처 : https://www.acmicpc.net/problem/2688
  * 이름 : 줄어들지 않아
  * 사용 알고리즘 : 다이나믹 프로그래밍
+ * 아까 문제랑 똑같은데 숫자가 여러 개.
  */
 public class BOJ2688 {
 
@@ -25,35 +27,38 @@ public class BOJ2688 {
     }
 
     private String solution(int [] arr) {
-
         StringBuilder builder = new StringBuilder();
         boolean first=true;
+
+        Long[][] memoi = new Long[65][10];
+        Arrays.fill(memoi[1],1L);// 1자리일 때 1로 채움.
+        this.iteration(memoi);
+
         for(int val: arr){
             if(first) first=false;
             else builder.append('\n');
-            builder.append(this.iteration(val));
+            builder.append(this.sum(memoi[val]));
         }
         return builder.toString();
     }
 
-    private long iteration(int val) {
-        long result = 0L;
-        long[][] memoi = new long[val + 1][10];
+    private Long sum(Long[] memoi) {
+        Long result = 0L;
+        for (Long l : memoi) {
+            result += l;
+        }
+        return result;
+    }
 
-        memoi[1] = new long[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-        for (int i = 2; i <= val; i++) {
-            int sum = 0;
+    private void iteration(Long[][] memoi) {
+        for (int i = 2; i < memoi.length; i++) {
+            Long sum = 0L;
             for (int j = 9; j >= 0; j--) {
                 sum += memoi[i - 1][j];
                 memoi[i][j] = sum;
             }
+            System.out.println("메모이 보여주기"+i+Arrays.toString(memoi[i]));
         }
-
-        for (long l : memoi[val]) {
-            result +=  l;
-        }
-        
-        return result;
     }
 
 }
