1d4a248c990f3de032630b8538f0a0b68a2ee4c3
Dev.Jusa <dev.jusa@gmail.com>
2021-12-03 20:14:50 +0900

    Solve 1967-상현

---
+++ sanghyeon/bfs dfs/BOJ1967.java
@@ -0,0 +1,65 @@
+package year_2021.month_12.day_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ1967 {
+    static ArrayList<Node> list[];
+    static int n;
+    static int max = 0;
+    static boolean visited[];
+    static int max_idx = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        list = new ArrayList[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            String[] t = br.readLine().split(" ");
+            int parent = Integer.parseInt(t[0]);
+            int child = Integer.parseInt(t[1]);
+            int weight = Integer.parseInt(t[2]);
+            list[parent].add(new Node(child, weight));
+            list[child].add(new Node(parent, weight));
+        }
+
+        visited = new boolean[n + 1];
+        visited[1] = true;
+        dfs(1, 0);
+
+        visited = new boolean[n + 1];
+        visited[max_idx] = true;
+        dfs(max_idx, 0);
+        System.out.println(max);
+    }
+
+    public static void dfs(int idx, int cnt) {
+        if (max < cnt) {
+            max = cnt;
+            max_idx = idx;
+        }
+        for (Node a : list[idx]) {
+            if (!visited[a.idx]) {
+                visited[a.idx] = true;
+                dfs(a.idx, cnt + a.cnt);
+            }
+        }
+    }
+
+    static class Node {
+        int idx, cnt;
+
+        Node(int idx, int cnt) {
+            this.idx = idx;
+            this.cnt = cnt;
+        }
+    }
+}
+
