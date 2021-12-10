3ccfc0641d02fa371f991ac348771a5aea12fbc0
PiaoZhengxun <pjh951218@naver.com>
2021-12-11 02:43:39 +0900

    Solve 1012-박정훈

---
+++ junghoon/dfsbfs/BOJ1012.java
@@ -0,0 +1,162 @@
+package dfsbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ1012 {
+	/* 유기농 배추
+	 * 
+	 * 문제: 차세대 영농인 한나는 강원도 고랭지에서 유기농 배추를 재배하기로 하였다. 
+	 *      농약을 쓰지 않고 배추를 재배하려면 배추를 해충으로부터 보호하는 것이 중요하기 때문에, 
+	 *      한나는 해충 방지에 효과적인 배추흰지렁이를 구입하기로 결심한다. 
+	 *      이 지렁이는 배추근처에 서식하며 해충을 잡아 먹음으로써 배추를 보호한다. 
+	 *      특히, 어떤 배추에 배추흰지렁이가 한 마리라도 살고 있으면 이 지렁이는 인접한 다른 배추로 이동할 수 있어, 
+	 *      그 배추들 역시 해충으로부터 보호받을 수 있다. 
+	 *      한 배추의 상하좌우 네 방향에 다른 배추가 위치한 경우에 서로 인접해있는 것이다.
+	 *      한나가 배추를 재배하는 땅은 고르지 못해서 배추를 군데군데 심어 놓았다. 
+	 *      배추들이 모여있는 곳에는 배추흰지렁이가 한 마리만 있으면 되므로 서로 인접해있는 배추들이 몇 군데에 퍼져있는지 조사하면 총 몇 마리의 지렁이가 필요한지 알 수 있다. 
+	 *      예를 들어 배추밭이 아래와 같이 구성되어 있으면 최소 5마리의 배추흰지렁이가 필요하다. 
+	 *      0은 배추가 심어져 있지 않은 땅이고, 1은 배추가 심어져 있는 땅을 나타낸다.
+	 * 
+	 * 입력: 입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 
+	 *      그 다음 줄부터 각각의 테스트 케이스에 대해 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 
+	 *      세로길이 N(1 ≤ N ≤ 50), 그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다. 
+	 *      그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다. 
+	 *      두 배추의 위치가 같은 경우는 없다.
+	 * 
+	 * 출력: 각 테스트 케이스에 대해 필요한 최소의 배추흰지렁이 마리 수를 출력한다.
+	 * 
+	 * 풀이: 방문 배열을 만들어서 배추가 있는 곳에서 방문하지 않았으면 count 1증가후 DFS 탐색
+	 *      DFS 탐색에서 4방면을 확인하면서 배추가 있고 방문하지 않으면 DFS 탐색 후에 배추밭을 다 탐색
+	 *     
+	 * */
+	
+	static int M, N, K;
+	static int[][] cabbage;
+	static boolean[][] visit;
+	static int count;
+	static int[] dx = { 0, -1, 0, 1 };
+	static int[] dy = { 1, 0, -1, 0 };
+
+	static void dfs(int x, int y) {
+		visit[x][y] = true;
+
+		for (int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
+
+			if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
+				if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
+					dfs(cx, cy);
+				}
+			}
+
+		}
+
+	}
+	
+	static void bfs(int x, int y) {
+		Queue<int[]> qu = new LinkedList<int[]>();
+		qu.add(new int[] { x, y });
+
+		while (!qu.isEmpty()) {
+			x = qu.peek()[0];
+			y = qu.peek()[1];
+			visit[x][y] = true;
+			qu.poll();
+			for (int i = 0; i < 4; i++) {
+				int cx = x + dx[i];
+				int cy = y + dy[i];
+
+				if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
+					if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
+						qu.add(new int[] { cx, cy });
+						visit[cx][cy] = true;
+					}
+				}
+
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+//		public void dfs() {
+			
+			int tc = Integer.parseInt(br.readLine());
+
+			for (int i = 0; i < tc; i++) {
+				count = 0;
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				M = Integer.parseInt(st.nextToken());
+				N = Integer.parseInt(st.nextToken());
+				cabbage = new int[M][N];
+				visit = new boolean[M][N];
+
+				K = Integer.parseInt(st.nextToken());
+				for (int j = 0; j < K; j++) {
+					st = new StringTokenizer(br.readLine(), " ");
+					int p1 = Integer.parseInt(st.nextToken());
+					int p2 = Integer.parseInt(st.nextToken());
+					cabbage[p1][p2] = 1;
+				}
+
+				for (int x = 0; x < M; x++) {
+					for (int y = 0; y < N; y++) {
+						if (cabbage[x][y] == 1 && !visit[x][y]) {
+							dfs(x, y);
+							count++;
+						}
+					}
+				}
+
+				System.out.println(count);
+			}
+
+		}
+//		}
+	
+//	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//	
+//	int T = Integer.parseInt(br.readLine());
+//
+//	for (int c = 0; c < T; c++) {
+//		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//		 M = Integer.parseInt(st.nextToken());
+//		 N = Integer.parseInt(st.nextToken());
+//		 K = Integer.parseInt(st.nextToken());
+//		cabbage = new int[M][N];
+//		visit = new boolean[M][N];
+//		count=0;
+//
+//		for (int i = 0; i < K; i++) {
+//			st=new StringTokenizer(br.readLine()," ");
+//			int p1 = Integer.parseInt(st.nextToken());
+//			int p2 = Integer.parseInt(st.nextToken());
+//			cabbage[p1][p2] = 1;
+//
+//		}
+//
+//		for (int i = 0; i < M; i++) {
+//			for (int j = 0; j < N; j++) {
+//				if (cabbage[i][j] == 1 && !visit[i][j]) {
+//					bfs(i, j);
+//					count++;
+//				}
+//			}
+//		}
+//		System.out.println(count);
+//	}
+	
+
+
+		
+
+}
